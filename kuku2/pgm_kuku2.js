09108bf4842bb0a3bb3861e0002b51ec94b55b1f
{gakuapu} <{dotddot.info@gmail.com}>
2022-11-05 19:17:58 +0900

    record added

--- kuku2/pgm_kuku2.js
+++ kuku2/pgm_kuku2.js
@@ -20,6 +20,20 @@ function generateRandoms(){
     };
 };
 
+let records = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+
+//要リファクタ↓
+const $r1 = document.getElementById("r1");
+const $r2 = document.getElementById("r2");
+const $r3 = document.getElementById("r3");
+const $r4 = document.getElementById("r4");
+const $r5 = document.getElementById("r5");
+const $r6 = document.getElementById("r6");
+const $r7 = document.getElementById("r7");
+const $r8 = document.getElementById("r8");
+const $r9 = document.getElementById("r9");
+//要リファクタ↑
+
 const $dan = document.getElementById("dan");
 const $startBtn = document.getElementById("start-btn");
 const $eraseBtn = document.getElementById("erase-btn");
@@ -31,7 +45,6 @@ const $kotae = document.getElementById("kotae");
 
 let danInt = 0;
 let count = 0;
-let n = 0;
 let a = 0;
 let countMax = 9;
 
@@ -53,7 +66,6 @@ function defaultdisplay(){
 
 function defaultlet(){
     count = 0;
-    n = 0;
     randoms.length = 0; //配列の要素を全て削除
     $hint0.innerText = "";
     $mondai.innerText = "";
@@ -62,6 +74,67 @@ function defaultlet(){
 
 function closing(){
     audio2.play();
+    if (records[danInt - 1] < 3){
+        records[danInt - 1]++;
+    };
+    //要リファクタ↓
+    $r1.innerText = ``;
+    $r2.innerText = ``;
+    $r3.innerText = ``;
+    $r4.innerText = ``;
+    $r5.innerText = ``;
+    $r6.innerText = ``;
+    $r7.innerText = ``;
+    $r8.innerText = ``;
+    $r9.innerText = ``;
+
+    let i;
+    if (records[0] > 0){
+        for (i = 0; i < records[0]; i++){
+            $r1.innerText += `☆ `;
+        }
+    };
+    if (records[1] > 0){
+        for (i = 0; i < records[1]; i++){
+            $r2.innerText = `☆ `;
+        }
+    };
+    if (records[2] > 0){
+        for (i = 0; i < records[2]; i++){
+            $r3.innerText += `☆ `;
+        }
+    };
+    if (records[3] > 0){
+        for (i = 0; i < records[3]; i++){
+            $r4.innerText += `☆ `;
+        }
+    };
+    if (records[4] > 0){
+        for (i = 0; i < records[4]; i++){
+            $r5.innerText += `☆ `;
+        }
+    };
+    if (records[5] > 0){
+        for (i = 0; i < records[5]; i++){
+            $r6.innerText += `☆ `;
+        }
+    };
+    if (records[6] > 0){
+        for (i = 0; i < records[6]; i++){
+            $r7.innerText += `☆ `;
+        }
+    };
+    if (records[7] > 0){
+        for (i = 0; i < records[7]; i++){
+            $r8.innerText += `☆ `;
+        }
+    };
+    if (records[8] > 0){
+        for (i = 0; i < records[8]; i++){
+            $r9.innerText += `☆ `;
+        }
+    };
+    //要リファクタ↑
     defaultlet();
     defaultdisplay();
     alert(`クリアしました`);
