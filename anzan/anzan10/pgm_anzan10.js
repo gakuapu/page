f00e874615c143f52da8c77f4564d3e59a56956a
{gakuapu} <{dotddot.info@gmail.com}>
2022-12-27 23:57:33 +0900

    anzan9 anzan10 added

---
+++ anzan/anzan10/pgm_anzan10.js
@@ -0,0 +1,154 @@
+function anzan10(){
+    
+    //1～countMaxまでのランダム配列（randoms）の生成
+    let countMax = 9;
+    let randoms = [];
+
+    function getRandom(min, max){
+    let randomNum = Math.floor(Math.random() * (max + 1 - min)) + min;
+    return randomNum;
+    };
+
+    function generateRandoms(){
+        for (let i = 1; i <= countMax; i++){
+            while(true){
+                let rtmp = getRandom(1, countMax);
+                if(!randoms.includes(rtmp)){
+                    randoms.push(rtmp);
+                    break;
+                }
+            } 
+        };
+    };
+
+    const $step = document.getElementById("step");
+    const $startBtn = document.getElementById("start-btn");
+    const $eraseBtn = document.getElementById("erase-btn");
+    const $resetBtn = document.getElementById("reset-btn");
+
+    const $mondai = document.getElementById("mondai");
+    const $kotae = document.getElementById("kotae");
+    
+    let count = 0;
+    let a = 0;
+    
+    function defaultlet(){
+        count = 0;
+        randoms.length = 0; //配列の要素を全て削除
+        a = 0;
+        $kotae.value = "";
+        $mondai.innerText = "";
+    };
+
+    function switchdisplay(){
+        $kotae.style.display = "inline-block";
+        $eraseBtn.style.display = "inline-block";
+        $resetBtn.style.display = "inline-block";
+        $step.style.display = "none";
+        $startBtn.style.display = "none";
+    };
+
+    function defaultdisplay(){
+        $kotae.style.display = "none";
+        $eraseBtn.style.display = "none";
+        $resetBtn.style.display = "none";
+        $step.style.display = "block";
+        $startBtn.style.display = "block";
+    };
+
+    function closing(){
+        audio2.play();
+        defaultdisplay();
+        defaultlet();
+        alert(`クリアしました`);
+    };
+
+    function switchSetup(stepValue){
+        switch(stepValue){
+            case "1":
+                step1Setup();
+                break;
+            case "2":
+                step2Setup();
+                break;
+            case "3":
+                step3Setup();
+                break;
+            case "4":
+                step4Setup();
+                break;
+            default:
+                alert(`リロードして下さい`);
+        };
+    };
+
+    function checkAnswer(){
+        if ($kotae.value == a){
+            setTimeout(() => {
+                audio1.play();
+                $kotae.value = "";
+            }, 500);
+            count++;
+            switchSetup($step.value);
+        };
+    };
+
+    function step1Setup(){
+        if (count < countMax){
+            a = 12 * (count + 1);
+            $mondai.innerText = 12 + `×` + (count + 1) + `＝`;
+        } else if (count == countMax){
+            closing();
+        };
+    };
+
+    function step2Setup(){
+        if (count < countMax){
+            a = 15 * (count + 1);
+            $mondai.innerText = 15 + `×` + (count + 1) + `＝`;
+        } else if (count == countMax){
+            closing();
+        };
+    };
+
+    function step3Setup(){
+        if (count < countMax){
+            a = 12 * randoms[count];
+            $mondai.innerText = 12 + `×` + randoms[count] + `＝`;
+        } else if (count == countMax){
+            closing();
+        };
+    };
+
+    function step4Setup(){
+        if (count < countMax){
+            a = 15 * randoms[count];
+            $mondai.innerText = 15 + `×` + randoms[count] + `＝`;
+        } else if (count == countMax){
+            closing();
+        };
+    };
+
+    $kotae.addEventListener("input", () => {
+        checkAnswer();
+    });
+
+    $startBtn.addEventListener("click", () => {
+        switchdisplay();
+        defaultlet();
+        generateRandoms();
+        switchSetup($step.value);
+    });
+    
+    $eraseBtn.addEventListener("click", () => {
+        $kotae.value = "";
+    });
+    
+    $resetBtn.addEventListener("click", () => {
+        defaultdisplay();
+        defaultlet();
+    });
+    
+};
+    
+anzan10();
---
+++ anzan/anzan9/pgm_anzan9.js
@@ -0,0 +1,139 @@
+function anzan9(){
+    
+    const $startBtn = document.getElementById("start-btn");
+    const $hintBtn = document.getElementById("hint-btn");
+    const $eraseBtn = document.getElementById("erase-btn");
+    const $resetBtn = document.getElementById("reset-btn");
+
+    const $mondai = document.getElementById("mondai");
+    const $hint = document.getElementById("hint");
+    const $kotae = document.getElementById("kotae");
+    
+    const $progress = document.getElementById("progress");
+    
+    let count = 0;
+    let n1 = 0;
+    let n11 = 0;
+    let n12 = 0;
+    let n2 = 0;
+    let n21 = 0;
+    let n22 = 0;
+    let a = 0;
+    let p = 0;
+    let pb = 0;
+    let mondaiNum = 10;
+    
+    function getRandom(min, max){
+        let randomNum = Math.floor(Math.random() * (max + 1 - min)) + min;
+        return randomNum;
+    };
+    
+    function defaultlet(){
+        count = 0;
+        n1 = 0;
+        n11 = 0;
+        n12 = 0;
+        n2 = 0;
+        n21 = 0;
+        n22 = 0;
+        a = 0;
+        p = 0;
+        pb = 0;
+        $kotae.value = "";
+        $mondai.innerText = "";
+        $hint.innerText = "";
+    };
+
+    function switchdisplay(){
+        $kotae.style.display = "block";
+        $progress.style.display = "block";
+        $eraseBtn.style.display = "inline-block";
+        $resetBtn.style.display = "inline-block";
+        $startBtn.style.display = "none";
+        $hintBtn.style.display = "block";
+    };
+
+    function defaultdisplay(){
+        $kotae.style.display = "none";
+        $progress.style.display = "none";
+        $eraseBtn.style.display = "none";
+        $resetBtn.style.display = "none";
+        $startBtn.style.display = "block";
+        $hintBtn.style.display = "none";
+    };
+
+    function closing(){
+        audio2.play();
+        $progress.value = 1;
+        defaultdisplay();
+        defaultlet();
+        alert(`クリアしました`);
+    };
+
+    function setup(){
+        if (count < mondaiNum){
+            pb = p;
+            p = getRandom(1, 5);
+            while (p == pb){
+                p = getRandom(1, 5);
+            }
+            if (p == 1){
+                n11 = getRandom(3, 9);
+                n21 = getRandom(2, n11 - 1);
+                n12 = getRandom(3, 9);
+                n22 = getRandom(2, n12 - 1);
+                n1 = n11 * 10 + n12;
+                n2 = n21 * 10 + n22;
+                a = n1 - n2;
+            } else {
+                n11 = getRandom(4, 9);
+                n21 = getRandom(2, n11 - 2);
+                n12 = getRandom(0, 8);
+                n22 = getRandom (n12 + 1, 9);
+            };
+            n1 = n11 * 10 + n12;
+            n2 = n21 * 10 + n22;
+            a = n1 - n2;
+            $mondai.innerText = n1 + `－` + n2 + `＝`;
+        } else if (count == mondaiNum){
+            closing();
+        };
+    };
+
+    $kotae.addEventListener("input", () => {
+        if ($kotae.value == a){
+            setTimeout(() => {
+                audio1.play();
+                $kotae.value = "";
+                $hint.innerText = "";
+                $progress.value = count / mondaiNum;
+            }, 500);
+            count++;
+            setup();
+        };
+    });
+    
+    $hintBtn.addEventListener("click", () => {
+        $hint.innerText = (n11 * 10) + `－` + (n21 * 10) + `は... ` + n12 + `－` + n22 + `は... くり下がりは...`;
+    });
+
+    $startBtn.addEventListener("click", () => {
+        switchdisplay();
+        defaultlet();
+        $progress.value = 0;
+        setup();
+    });
+    
+    $eraseBtn.addEventListener("click", () => {
+        $kotae.value = "";
+    });
+    
+    $resetBtn.addEventListener("click", () => {
+        defaultdisplay();
+        defaultlet();
+        $progress.value = 0;
+    });
+    
+};
+    
+anzan9();
