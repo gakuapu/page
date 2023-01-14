a5345f55bfc903fe01a9ca2b738514f705f85bb5
{gakuapu} <{dotddot.info@gmail.com}>
2023-01-15 01:39:24 +0900

    anzan16 added

--- 2nen/kuku4/pgm_kuku4.js
+++ 2nen/kuku4/pgm_kuku4.js
@@ -5,7 +5,7 @@ const $hintBtn = document.getElementById("hint-btn");
 const $hint = document.getElementById("hint");
 
 $hintBtn.addEventListener("click", () => {
-    $hint.innerText = `ヒント：よこから見ると？たてから見ると？`;
+    $hint.innerText = `ヒント：よこから先にかぞえると？たてから先にかぞえると？`;
     $hintBtn.style.display = "none";
 });
 
