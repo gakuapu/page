232fe39a031d038f5971863ef016afeac6c9fba3
{gakuapu} <{dotddot.info@gmail.com}>
2024-07-28 00:13:43 +0900

    en updating

--- en/pgm_en.js
+++ en/pgm_en.js
@@ -1,8 +1,10 @@
-function en11_1() {
+function en1() {
     
-    const $menuBtn = document.getElementById("menu-btn");
-    const $startEBtn = document.getElementById("startE-btn");
-    const $startJBtn = document.getElementById("startJ-btn");
+    const $topBtn = document.getElementById("top-btn");
+    const $stopBtn = document.getElementById("stop-btn");
+    const $lng = document.getElementById("lng");
+    const $mode = document.getElementById("mode");
+    const $startBtn = document.getElementById("start-btn");
     const $mainbox = document.getElementById("mainbox");
     const $mainflame = document.getElementById("mainflame");
     const $comp = document.getElementById("comp");
@@ -12,55 +14,120 @@ function en11_1() {
     const $appearBtn = document.getElementById("appear-btn");
     const $wordtable = document.getElementById("wordtable");
 
-    let startEorJ = 0; //EtoJなら0, JtoEなら1
-    let currentEorJ = 0; //Eなら0, Jなら1
+    let currentlng = 0; //EtoJなら0, JtoEなら1
+    let mode = 0;
     let cardnum = 0;
     let cardnumMax = 0;
+    let tgttext = "";
+    let tgtnum = 0;
 
     let worddata = [];
 
-    for (let row of $wordtable.rows) {
+    function gettable () {
+        for (let row of $wordtable.rows) {
         worddata.push(row.innerText.split('\t'));
+        };
     };
 
-    $menuBtn.addEventListener("click", () => {
+    $topBtn.addEventListener("click", () => {
         window.location.href = "https://gakuapu.github.io/page/top_en.html";
     });
 
-    $startEBtn.addEventListener("click", () => {
-        $mainflame.style.display = "block";
-        $mainbox.style.display = "block";
-        $comp.style.display = "none";
+    function compdisplay () {
+        defaultdisplay ();
+        $comp.style.display = "block";
+    };
+
+    function defaultdisplay () {
+        $mainflame.style.display = "none";
+        $mainbox.style.display = "none";
+        $stopBtn.style.display = "none";
+        $topBtn.style.display = "block";
         $okBtn.style.display = "none";
         $againBtn.style.display = "none";
-        $startEBtn.style.display = "none";
-        $startJBtn.style.display = "none";
-        $mainbox.innerText = worddata[0][0];
-        startEorJ = 0;
-    });
-
-    $startJBtn.addEventListener("click", () => {
-        $mainflame.style.display = "block";
-        $mainbox.style.display = "block";
+        $lng.style.display = "inline-block";
+        $mode.style.display = "inline-block";
+        $startBtn.style.display = "block";
         $comp.style.display = "none";
-        $okBtn.style.display = "none";
-        $againBtn.style.display = "none";
-        $startEBtn.style.display = "none";
-        $startJBtn.style.display = "none";
-        $mainbox.innerText = worddata[0][1];
-        startEorJ = 1;
+        currentlng = 0;
+        mode = 0;
+        cardnum = 0;
+        cardnumMax = 0;
+        tgttext = "";
+        tgtnum = 0;
+        worddata.length = 0;
+    };
+ 
+    $stopBtn.addEventListener("click", () => {
+        defaultdisplay ();
     });
 
-    $mainbox.addEventListener("click", () => {
-        $okBtn.style.display = "inline-block";
-        $againBtn.style.display = "inline-block";
-        if (currentEorJ == 0) {
+    function modesetup2a () {
+        if ($lng.value == 0) {
+            $mainbox.innerText = worddata[cardnum][0];
+        } else {
             $mainbox.innerText = worddata[cardnum][1];
-            currentEorJ = 1;
+        };
+        setTimeout (modesetup2b, 3000 / mode);
+    };
+
+    function modesetup2b () {
+        if ($lng.value == 0) {
+            $mainbox.innerText = worddata[cardnum][1];
         } else {
             $mainbox.innerText = worddata[cardnum][0];
-            currentEorJ = 0;
         };
+        cardnum++;
+        if (cardnum < cardnumMax) {
+            setTimeout (modesetup2a, 3000 / mode);
+        } else {
+            setTimeout(() => compdisplay(), 3000 / mode);
+        };
+    };
+
+    function modesetup () {
+        if (mode != 0) {
+            $againBtn.style.display = "inline-block"
+            modesetup2a ();
+        };
+    };
+
+    $mainbox.addEventListener("click", () => { //こっちを次に進むにする
+        if (mode == 0) {
+            $okBtn.style.display = "inline-block";
+            $againBtn.style.display = "inline-block";
+            if (currentlng == 0) {
+                $mainbox.innerText = worddata[cardnum][1];
+                currentlng = 1;
+            } else {
+                $mainbox.innerText = worddata[cardnum][0];
+                currentlng = 0;
+            };
+        };
+    });
+
+    $startBtn.addEventListener("click", () => {
+        gettable ();
+        console.log(worddata);
+        mode = $mode.value;
+        $mainflame.style.display = "block";
+        $mainbox.style.display = "block";
+        $startBtn.style.display = "none";
+        $topBtn.style.display = "none";
+        $stopBtn.style.display = "block";
+        $comp.style.display = "none";
+        $okBtn.style.display = "none";
+        $againBtn.style.display = "none";
+        $lng.style.display = "none";
+        $mode.style.display = "none";
+        if ($lng.value == 0) {
+            $mainbox.innerText = worddata[0][0];
+        } else {
+            $mainbox.innerText = worddata[0][1];
+        };
+        currentlng = $lng.value;
+        cardnumMax = worddata.length;
+        modesetup ();
     });
 
     $okBtn.addEventListener("click", () => {
@@ -69,47 +136,65 @@ function en11_1() {
         cardnum++;
         cardnumMax = worddata.length;
         if (cardnum == cardnumMax) {
-            $mainflame.style.display = "none";
-            $mainbox.style.display = "none";
-            $comp.style.display = "block";
-            $startEBtn.style.display = "inline-block";
-            $startJBtn.style.display = "inline-block";
-            cardnum = 0; 
+            compdisplay ();
         } else {
-
-            if (startEorJ == 0) {
+            if ($lng.value == 0) {
                 $mainbox.innerText = worddata[cardnum][0];
+                currentlng = 0;
             } else {
                 $mainbox.innerText = worddata[cardnum][1];
+                currentlng = 1;
             };
         };
     });
 
+    function findRow (worddata, tgttext) {
+        for (let tgtrow = 0; tgtrow < worddata.length; tgtrow++) {
+            for (let tgtcol = 0; tgtcol < worddata[tgtrow].length; tgtcol++) {
+                if (worddata[tgtrow][tgtcol] === tgttext) {
+                    return tgtrow;
+                };
+            };
+        };
+    };
+
     $againBtn.addEventListener("click", () => {
-        $okBtn.style.display = "none";
-        $againBtn.style.display = "none";
-        worddata.push(worddata[cardnum]);
-        cardnum++;
-        cardnumMax = worddata.length;
-        if (startEorJ == 0) {
-            $mainbox.innerText = worddata[cardnum][0];
+        if (mode == 0) {
+            worddata.push(worddata[cardnum]);
+            cardnum++;
+            cardnumMax = worddata.length;
+            $okBtn.style.display = "none";
+            $againBtn.style.display = "none";
+            if ($lng.value == 0) {
+                $mainbox.innerText = worddata[cardnum][0];
+                currentlng = 0;
+            } else {
+                $mainbox.innerText = worddata[cardnum][1];
+                currentlng = 1;
+            };
         } else {
-            $mainbox.innerText = worddata[cardnum][1];
+            tgttext = $mainbox.innerText;
+            console.log(tgttext);
+            tgtnum = findRow (worddata, tgttext);
+            console.log(tgtnum);
+            worddata.push(worddata[tgtnum]);
+            console.log(worddata);
+            cardnumMax = worddata.length;
         };
     });
 
     $hideBtn.addEventListener("click", () => {
+        $wordtable.style.opacity = 0;
         $hideBtn.style.display = "none";
         $appearBtn.style.display = "inline-block";
-        $wordtable.style.display = "none";
     });
 
     $appearBtn.addEventListener("click", () => {
+        $wordtable.style.opacity = 1;
         $hideBtn.style.display = "inline-block";
         $appearBtn.style.display = "none";
-        $wordtable.style.display = "block";
     });
 
 };
 
-en11_1();
+en1();
