f1f42c8a10c4bce830a22a64075a31a66f8edd98
{gakuapu} <{dotddot.info@gmail.com}>
2024-08-13 19:27:33 +0900

    top modified

--- en/pgm_top_en.js
+++ en/pgm_top_en.js
@@ -1,14 +1,22 @@
 function topen() {
     
-    const $grade = document.getElementById("grade");
-    const $step = document.getElementById("step");
-    const $startBtn = document.getElementById("start-btn");
-
     let dref = "";
 
-    $startBtn.addEventListener("click", () => {
-        //dref = "https://gakuapu.github.io/page/en" + $grade.value + "_" + $step.value + ".html";
-        dref = "en" + $grade.value + "_" + $step.value + ".html";
+    const $grade1 = document.getElementById("grade1");
+    const $step1 = document.getElementById("step1");
+    const $startBtn1 = document.getElementById("start-btn1");
+
+    $startBtn1.addEventListener("click", () => {
+        dref = "en" + $grade1.value + "_" + $step1.value + ".html";
+        window.location.href = dref;
+    });
+
+    const $grade2 = document.getElementById("grade2");
+    const $step2 = document.getElementById("step2");
+    const $startBtn2 = document.getElementById("start-btn2");
+
+    $startBtn2.addEventListener("click", () => {
+        dref = "en" + $grade2.value + "_" + $step2.value + ".html";
         window.location.href = dref;
     });
 
