012548b7c68926385b41d8c3e25887dcaac64585
{gakuapu} <{dotddot.info@gmail.com}>
2024-08-18 14:00:48 +0900

    117t

---
+++ en_test/wordlist11_7t.js
@@ -0,0 +1,21 @@
+const $list = [
+    ["fish", "魚"],
+    ["fruit", "フルーツ"],
+    ["grape", "ぶどう"],
+    ["hat", "帽子"],
+    ["head", "頭"],
+    ["homework", "宿題"],
+    ["horse", "馬"],
+    ["hospital", "病院"],
+    ["jam", "ジャム"],
+    ["king", "王さま"],
+    ["lemon", "レモン"],
+    ["lion", "ライオン"],
+    ["milk", "牛乳"],
+    ["monkey", "猿"],
+    ["mouse", "ネズミ"],
+    ["mouth", "口"],
+    ["noodle", "麺"],
+    ["nose", "鼻"],
+    ["notebook", "ノート"]
+];
