e86444dfee696baac62e6694fa493c36d0ec6ad9
{gakuapu} <{dotddot.info@gmail.com}>
2024-08-25 22:28:54 +0900

    123t

---
+++ en_test/wordlist12_3t.js
@@ -0,0 +1,22 @@
+const $list = [
+    ["father", "父"],
+    ["flower", "花"],
+    ["food", "食べ物"],
+    ["friend", "友達"],
+    ["girl", "女の子"],
+    ["group", "グループ"],
+    ["hand", "手"],
+    ["home", "家(に)"],
+    ["house", "家"],
+    ["Japan", "日本"],
+    ["letter", "手紙"],
+    ["library", "図書館"],
+    ["light", "光/明るい/軽い"],
+    ["lunch", "昼食"],
+    ["man", "男性"],
+    ["money", "お金"],
+    ["morning", "朝"],
+    ["mother", "母"],
+    ["mountain", "山"],
+    ["music", "音楽"]
+];
