038fe96a80e083f807dd0aef72fc22965f038ed7
{gakuapu} <{dotddot.info@gmail.com}>
2024-09-04 22:25:56 +0900

    215

---
+++ en_test/wordlist21_5t.js
@@ -0,0 +1,19 @@
+const $list = [
+    ["autumn", "秋"],
+    ["baseball", "野球"],
+    ["basketball", "バスケットボール"],
+    ["building", "建物"],
+    ["care", "世話、注意/～を気にかける"],
+    ["college", "大学"],
+    ["earth", "地球"],
+    ["fall", "秋"],
+    ["forest", "森"],
+    ["Japanese", "日本人/日本語/日本の"],
+    ["map", "地図"],
+    ["math", "数学"],
+    ["message", "メッセージ"],
+    ["movie", "映画"],
+    ["museum", "博物館、美術館"],
+    ["plant", "植物"],
+    ["road", "道路"]
+];
