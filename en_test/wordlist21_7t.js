6b63897b7f7ab4113b75d45212892edfc51ff150
{gakuapu} <{dotddot.info@gmail.com}>
2024-09-04 22:43:37 +0900

    217

---
+++ en_test/wordlist21_7t.js
@@ -0,0 +1,22 @@
+const $list = [
+    ["season", "季節"],
+    ["ski", "スキー"],
+    ["speech", "スピーチ"],
+    ["soccer", "サッカー"],
+    ["sound", "音 / ～に聞こえる"],
+    ["sport", "スポーツ"],
+    ["spring", "春"],
+    ["store", "店"],
+    ["summer", "夏"],
+    ["supermarket", "スーパー"],
+    ["swimming", "水泳"],
+    ["taxi", "タクシー"],
+    ["tennis", "テニス"],
+    ["theater", "映画館、劇場"],
+    ["university", "大学"],
+    ["volleyball", "バレーボール"],
+    ["way", "道 / 方法"],
+    ["winter", "冬"],
+    ["world", "世界"],
+    ["zoo", "動物園"]
+];
