4b5bcd80280cfbf7835f28428fcc88e38561511d
{gakuapu} <{dotddot.info@gmail.com}>
2024-09-14 19:15:52 +0900

    222

--- en_test/wordlist22_10t.js
+++ en_test/wordlist22_10t.js
@@ -5,7 +5,7 @@ const $list = [
     ["herself", "彼女自身(を/に)"],
     ["itself", "それ自体(を/に)"],
     ["ourselves", "私たち自身(を/に)"],
-    ["themselves", "彼ら/彼女ら/それら自身(を/に)"],
+    ["themselves", "彼ら/彼女ら/それら自身"],
     ["above", "～の上に"],
     ["below", "～の下に"],
     ["behind", "～の後ろに"],
