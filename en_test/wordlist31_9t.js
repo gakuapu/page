b445d489803f9d08a0a083140fd0615760a29c8c
{gakuapu} <{dotddot.info@gmail.com}>
2024-09-23 15:44:36 +0900

    319

---
+++ en_test/wordlist31_9t.js
@@ -0,0 +1,22 @@
+const $list = [
+    ["officer", "役人"],
+    ["pamphlet", "パンフレット"],
+    ["patient", "患者、病人"],
+    ["period", "時代"],
+    ["pollution", "汚染"],
+    ["purpose", "目的"],
+    ["rhythm", "リズム"],
+    ["scene", "シーン、光景"],
+    ["site", "場所、遺跡"],
+    ["subject", "主題/テーマ、科目/教科"],
+    ["tail", "しっぽ"],
+    ["tournament", "トーナメント"],
+    ["traffic", "交通"],
+    ["treasure", "宝物"],
+    ["view", "景色/眺め、見解/考え"],
+    ["wheelchair", "車イス"],
+    ["unicycle", "一輪車"],
+    ["uniform", "ユニフォーム、制服"],
+    ["yogurt", "ヨーグルト"],
+    ["website", "ウェブサイト"]
+];
