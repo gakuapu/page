b241bda21ed54c402c96ba138322e7c4144c3beb
{gakuapu} <{dotddot.info@gmail.com}>
2024-09-23 15:01:21 +0900

    315

---
+++ en_test/wordlist31_5t.js
@@ -0,0 +1,22 @@
+const $list = [
+    ["attention", "注意、注目"],
+    ["billion", "10億"],
+    ["bomb", "爆弾"],
+    ["cafeteria", "カフェテリア、学食"],
+    ["career", "経歴、職業"],
+    ["customer", "顧客"],
+    ["championship", "大会 / 優勝"],
+    ["charity", "チャリティー、慈善"],
+    ["character", "性格・特色 / 人物 / 文字"],
+    ["convenience", "便利さ"],
+    ["corner", "コーナー、曲がり角"],
+    ["crane", "鶴"],
+    ["date", "日付"],
+    ["designer", "デザイナー、設計者"],
+    ["dolphin", "イルカ"],
+    ["dome", "ドーム"],
+    ["gram", "グラム"],
+    ["headache", "頭痛"],
+    ["industry", "産業"],
+    ["interest", "興味、関心 / 利子"]
+];
