0cfc6188a6ee280abac4cc0391046b120e5b8035
{gakuapu} <{dotddot.info@gmail.com}>
2024-10-01 23:35:24 +0900

    modified2

--- en_test/wordlist13_1t.js
+++ en_test/wordlist13_1t.js
@@ -1,18 +1,18 @@
 const $list = [
     ["crayon", "クレヨン"],
     ["cucumber", "きゅうり"],
-    ["dentist", "歯医者"],
     ["desk", "机"],
     ["dessert", "デザート"],
+    ["doll", "人形"],
     ["duck", "アヒル"],
     ["east", "東"],
     ["elephant", "象"],
-    ["energy", "エネルギー"],
     ["foot", "足"],
     ["fox", "キツネ"],
     ["frog", "カエル"],
+    ["glass", "ガラス / グラス"],
+    ["glasses", "メガネ"],
     ["gloves", "手袋"],
     ["goat", "ヤギ"],
-    ["gorilla", "ゴリラ"],
-    ["gym", "体育館"]
+    ["gorilla", "ゴリラ"]
 ];
