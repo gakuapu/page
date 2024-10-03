044a5a8e69a2e5e56c319cfcd0acddff51e8e174
{gakuapu} <{dotddot.info@gmail.com}>
2024-10-03 14:18:58 +0900

    325

---
+++ en_test/wordlist32_5t.js
@@ -0,0 +1,21 @@
+const $list = [
+    ["ambulance", "救急車"],
+    ["beauty", "美しさ"],
+    ["blackboard", "黒板"],
+    ["bubble", "泡"],
+    ["capital", "首都"],
+    ["cause", "原因/～の原因となる"],
+    ["chart", "図表"],
+    ["comment", "コメント"],
+    ["decision", "決定、決心"],
+    ["earthquake", "地震"],
+    ["engineer", "技術者、エンジニア"],
+    ["entrance", "入り口"],
+    ["favor", "お願い"],
+    ["film", "フィルム、映画"],
+    ["firework", "花火"],
+    ["generation", "世代 / 同世代の人々"],
+    ["locker", "ロッカー"],
+    ["manner", "マナー"],
+    ["model", "モデル(型、模型)"]
+];
