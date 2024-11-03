9aa9f27a4d490cd716f4472b9493bf1d1c18f431
{gakuapu} <{dotddot.info@gmail.com}>
2024-11-04 00:08:23 +0900

    一旦終了

--- sidebar_en.js
+++ sidebar_en.js
@@ -4,42 +4,7 @@ function sidebarEN () {
     .then(response => response.text())
     .then(html => {
         document.getElementById("en-sidebar-container").innerHTML = html;
-    })
-
-    const $enLinkContainer1 = document.getElementById("en-link-container1");
-    const $enLinkContainer2 = document.getElementById("en-link-container2");
-    const $enLinkContainer3 = document.getElementById("en-link-container3");
-    const $enLinkContainer4 = document.getElementById("en-link-container4");
-    let p_ensb = Math.floor(Math.random()) * 4;
-
-    switch (p_ensb) {
-        case 0:
-            $enLinkContainer1.style.display = "block";
-            $enLinkContainer2.style.display = "none";
-            $enLinkContainer3.style.display = "none";
-            $enLinkContainer4.style.display = "none";
-            break;
-        case 1:
-            $enLinkContainer1.style.display = "none";
-            $enLinkContainer2.style.display = "block";
-            $enLinkContainer3.style.display = "none";
-            $enLinkContainer4.style.display = "none";
-            break;
-        case 2:
-            $enLinkContainer1.style.display = "none";
-            $enLinkContainer2.style.display = "none";
-            $enLinkContainer3.style.display = "block";
-            $enLinkContainer4.style.display = "none";
-            break;
-        case 3:
-            $enLinkContainer1.style.display = "none";
-            $enLinkContainer2.style.display = "none";
-            $enLinkContainer3.style.display = "none";
-            $enLinkContainer4.style.display = "block";
-            break;
-        default:
-            alert("リロードして下さい");
-    };
+    });
 
     document.getElementById("en-menu-toggle").addEventListener("click", () => {
         const $enSidebar = document.getElementById("en-sidebar-container");
