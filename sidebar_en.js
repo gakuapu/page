869be43a5c2c36dd886e06c81aeb45d5390744a2
{gakuapu} <{dotddot.info@gmail.com}>
2024-11-03 14:05:15 +0900

    link修正

--- sidebar_en.js
+++ sidebar_en.js
@@ -1,6 +1,6 @@
 function sidebarEN () {
 
-    fetch("sidebar_en.html")
+    fetch("https://gakuapu.github.io/page/sidebar_en.html")
         .then(response => response.text())
         .then(html => {
             document.getElementById("en-sidebar-container").innerHTML = html;
