f72486135effae749788abc9676464c6b8450443
{gakuapu} <{dotddot.info@gmail.com}>
2024-11-10 11:34:31 +0900

    amzn test

--- sidebar_en.js
+++ sidebar_en.js
@@ -34,6 +34,24 @@ function sidebarEN () {
             <img border="0" width="300" height="250" alt="" src="https://www27.a8.net/svt/bgt?aid=200926309010&wid=002&eno=01&mid=s00000014879005016000&mc=1"></a>
             <img border="0" width="1" height="1" src="https://www14.a8.net/0.gif?a8mat=3BMJQD+5YDLM+36T2+TV3PD" alt="">
             `
+            ,
+            `
+            <div class="paapi5-pa-ad-unit pull-left">
+                <div class="paapi5-pa-product-container">
+                    <div class="paapi5-pa-product-image">
+                        <div class="paapi5-pa-product-image-wrapper">
+                            <a class="paapi5-pa-product-image-link" href="https://www.amazon.co.jp/dp/B0CWZ7X7CQ?tag=gakuapu-22&amp;linkCode=ogi&amp;th=1&amp;psc=1" title="N/A" target="_blank"></a>
+                            <img class="paapi5-pa-product-image-source" src="https://m.media-amazon.com/images/I/51lSQKbWfpL._SL500_.jpg" alt="N/A">
+                        </div>
+                    </div>
+                    <div class="paapi5-pa-product-details">
+                        <div class="paapi5-pa-product-title">
+                            <a class="paap5-pa-product-title-link" href="https://www.amazon.co.jp/dp/B0CWZ7X7CQ?tag=gakuapu-22&amp;linkCode=ogi&amp;th=1&amp;psc=1" title="N/A" target="_blank">【Amazon】ＮＨＫラジオ 中学生の基礎英語　レベル１ 2024年 11月号 ［雑誌］ (NＨＫテキスト)</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            `
         ];
 
         const isMobile_en = window.matchMedia(`(max-width: 1024px)`).matches;
