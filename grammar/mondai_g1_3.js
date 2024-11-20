68bc446a71bc8860011c7d8b59c48f33e01b94d5
{gakuapu} <{dotddot.info@gmail.com}>
2024-11-20 23:14:48 +0900

    g3

---
+++ grammar/mondai_g1_3.js
@@ -0,0 +1,14 @@
+const $list = [
+
+    ["私はピアノを弾くことができます。", "I can play the piano.", "I", "can", "play", "the", "piano", "am", "do", "I'm"],
+    ["あなたはとても速く泳げます。", "You can swim very fast.", "you", "can", "swim", "very", "fast", "are", "do", "a"],
+    ["彼らは上手に英語を話すことができます。", "They can speak English well.", "they", "can", "speak", "English", "well", "are", "do", "an"],
+    ["私の猫は泳げません。", "My cat can not swim.", "my", "cat", "can", "not", "swim", "a", "is", "I"],
+    ["私たちは公園で野球をすることができません。", "We can't play baseball in the park.", "we", "can't", "play", "baseball", "in", "the", "park", "don't"],
+    ["私たちはサッカーができません。", "We can not play soccer.", "we", "can", "not", "play", "soccer", "do", "are", "don't"],
+    ["私は上手に歌えません。", "I can't sing well.", "I", "can't", "sing", "well", "I'm", "do", "not", "am"],
+    ["彼らは英語を話せますか?", "Can they speak English?", "can", "they", "speak", "English", "are", "do", "the", "an"],
+    ["あなたは車を運転できますか?", "Can you drive a car?", "can", "you", "drive", "a", "car", "are", "do", "is"],
+    ["あなたは私の家に来ることができますか?", "Can you come to my house?", "can", "you", "come", "to", "my", "house", "do", "are"]
+
+]
