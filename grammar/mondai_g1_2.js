ad2eca55096c53b99818c088b94d65bd726bf230
{gakuapu} <{dotddot.info@gmail.com}>
2024-11-20 22:45:42 +0900

    g2

---
+++ grammar/mondai_g1_2.js
@@ -0,0 +1,14 @@
+const $list = [
+
+    ["私は電車で学校に行きます。", "I go to school by train.", "I", "go", "to", "school", "by", "train", "am", "I'm"],
+    ["彼らは毎日英語を勉強します。", "They study English every day.", "they", "study", "English", "every", "day", "am", "are", "is"],
+    ["私たちは水曜日に英語を勉強します。", "We study English on Wednesdays.", "we", "study", "English", "on", "Wednesdays", "are", "a", "an"],
+    ["私は日曜日は勉強しません。", "I don't study on Sundays.", "I", "don't", "study", "on", "Sundays", "am", "I'm", "not"],
+    ["彼らは日本語を話しません。", "They don't speak Japanese.", "they", "don't", "speak", "Japanese", "are", "not", "aren't", "a"],
+    ["トムと私は東京に住んでいません。", "Tom and I don't live in Tokyo.", "Tom", "and", "I", "don't", "live", "in", "Tokyo", "aren't"],
+    ["あなたは走るのが好きですか?", "Do you like running?", "do", "you", "like", "running", "are", "is", "am", "a"],
+    ["あなたは放課後、本を読みますか?", "Do you read a book after school?", "do", "you", "read", "a", "book", "after", "school", "are"],
+    ["あなたは野球が好きですか?", "Do you like baseball?", "do", "you", "like", "baseball", "are", "is", "am", "a"],
+    ["彼らは東京に住んでいますか?", "Do they live in Tokyo?", "do", "they", "live", "in", "Tokyo", "are", "is", "am"]
+    
+]
