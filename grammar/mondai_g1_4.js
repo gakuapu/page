379cc5c17452a3b95c72010a38cf691fcfd49f5e
{gakuapu} <{dotddot.info@gmail.com}>
2024-11-22 00:16:05 +0900

    g4

---
+++ grammar/mondai_g1_4.js
@@ -0,0 +1,16 @@
+const $list = [
+
+    ["あなたはいつピアノを弾きますか?", "When do you play the piano?", "when", "do", "you", "play", "the", "piano", "are", "what"],
+    ["トムはどこの出身ですか?", "Where is Tom from?", "where", "is", "Tom", "from", "are", "do", "what", "am"],
+    ["あなたは昼食に何を食べますか?", "What do you have for lunch?", "what", "do", "you", "have", "for", "lunch", "are", "is"],
+    ["あなたはどうやって学校に行きますか?", "How do you go to school?", "how", "do", "you", "go", "to", "school", "are", "what"],
+    ["今日の天気はどうですか?", "How is the weather today?", "how", "is", "the", "weather", "today", "are", "do", "what"],
+    ["あなたの先生は誰ですか?", "Who is your teacher?", "who", "is", "your", "teacher", "you", "are", "what", "do"],
+    ["あなたはどのくらい速く走ることができますか?", "How fast can you run?", "how", "fast", "can", "you", "run", "do", "are", "when"],
+    ["あれは誰の自転車ですか?", "Whose bicycle is that?", "whose", "bicycle", "is", "that", "who", "do", "are", "have"],
+    ["今、何時ですか?", "What time is it now?", "what", "time", "is", "it", "now", "do", "are", "when"],
+    ["どちらのネコがあなたのですか?", "Which cat is yours?", "which", "cat", "is", "yours", "who", "you", "are", "your"],
+    ["あなたは何本ペンを持っていますか?", "How many pens do you have?", "how", "many", "pens", "do", "you", "have", "are", "what"],
+    ["あなたは何語を話すことができますか?", "What languages can you speak?", "what", "languages", "can", "you", "speak", "do", "are", "how"]
+
+]
