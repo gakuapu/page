6971103674ff5b1bef2189294014020e83b1f05a
{gakuapu} <{dotddot.info@gmail.com}>
2024-11-24 15:41:11 +0900

    g0

---
+++ grammar/mondai_g1_0.js
@@ -0,0 +1,14 @@
+const $list = [
+
+    ["彼は私の友達です。", "He is my friend.", "he", "is", "my", "friend", "the", "an", "I", "a"],
+    ["あれは私のネコです。", "That is my cat.", "that", "is", "my", "cat", "a", "an", "the", "I"],
+    ["あれは象です。", "That is an elephant.", "that", "is", "an", "elephant", "am", "its", "I", "a"],
+    ["私は彼の弟ではありません。", "I am not his brother.", "I", "am", "not", "his", "brother", "the", "an", "a"],
+    ["私たちは先生ではありません。", "We are not teachers.", "we", "are", "not", "teachers", "a", "an", "the", "my"],
+    ["これは私の絵ではありません。", "This is not my picture.", "this", "is", "not", "my", "picture", "an", "the", "a"],
+    ["彼はあなたのお兄さんですか?", "Is he your brother?", "is", "he", "your", "brother", "a", "the", "you", "his"],
+    ["これは私の鉛筆ですか?", "Is this my pencil?", "is", "this", "my", "pencil", "a", "an", "the", "I"],
+    ["私は毎日、リンゴを1つ食べます。", "I have an apple every day.", "I", "have", "an", "apple", "every", "day", "a", "the"],
+    ["私は毎日、その本を読んでいます。", "I read the book every day.", "I", "read", "the", "book", "every", "day", "a", "an"]
+    
+]
