440a47554f347244c7b0dc75e1523d85cf4aacfc
{gakuapu} <{dotddot.info@gmail.com}>
2024-11-27 00:09:36 +0900

    g5

---
+++ grammar/mondai_g1_5.js
@@ -0,0 +1,16 @@
+const $list = [
+
+    ["トムは毎日、スペイン語の勉強をします。", "Tom studies Spanish every day.", "Tom", "studies", "Spanish", "every", "day", "study", "studys", "a"],
+    ["私の父は車を持っています。", "My father has a car.", "my", "father", "has", "a", "car", "have", "I", "haves"],
+    ["彼女はピアノを弾きます。", "She plays the piano.", "she", "plays", "the", "piano", "play", "plaies", "does", "a"],
+    ["私の弟は電話を持っていません。", "My brother doesn't have a phone.", "my", "brother", "doesn't", "have", "a", "phone", "don't", "has"],
+    ["彼は自転車に乗りません。", "He does not ride a bicycle.", "he", "does", "not", "ride", "a", "bicycle", "do", "rides"],
+    ["トムは読書がとても好きですか?", "Does Tom like reading very much?", "does", "Tom", "like", "reading", "very", "much", "do", "likes"],
+    ["彼女は今日、学校に来ますか?", "Does she come to school today?", "does", "she", "come", "to", "school", "today", "do", "comes"],
+    ["彼はいつも、どこで勉強をしますか?", "Where does he study every day?", "where", "does", "he", "study", "every", "day", "do", "studies"],
+    ["彼女は朝ごはんに何を食べますか?", "What does she have for breakfast?", "what", "does", "she", "have", "for", "breakfast", "do", "has"],
+    ["メアリーはとても速く泳ぐことができます。", "Mary can swim very fast.", "Mary", "can", "swim", "very", "fast", "swims", "cans", "does"],
+    ["彼らは日本語をとても上手に話します。", "They speak Japanese very well.", "they", "speak", "Japanese", "very", "well", "speaks", "a", "the"],
+    ["彼らはあなたに話しかけますか?", "Do they talk to you?", "do", "they", "talk", "to", "you", "does", "talks", "them"]
+
+]
