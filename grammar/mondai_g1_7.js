1dfb4328e3f2d744fed7236bf7d620b6254fc340
{gakuapu} <{dotddot.info@gmail.com}>
2024-12-01 00:43:41 +0900

    g7

---
+++ grammar/mondai_g1_7.js
@@ -0,0 +1,14 @@
+const $list = [
+
+    ["私は昨日、英語を勉強しました。", "I studied English yesterday.", "I", "studied", "English", "yesterday", "studyed", "study", "a", "an"],
+    ["私は2年前、京都に行きました。", "I went to Kyoto two years ago.", "I", "went", "to", "Kyoto", "two", "years", "ago", "goed"],
+    ["彼女は私とランチを食べました。", "She ate lunch with me.", "she", "ate", "lunch", "with", "me", "eated", "I", "eats"],
+    ["彼女は昨日、勉強していません。", "She did not study yesterday.", "she", "did", "not", "study", "yesterday", "does", "studies", "studied"],
+    ["私は今朝、朝食を食べていません。", "I didn't eat breakfast this morning.", "I", "didn't", "eat", "breakfast", "this", "morning", "ate", "don't"],
+    ["彼は昨日、野球をしましたか?", "Did he play baseball yesterday?", "did", "he", "play", "baseball", "yesterday", "plays", "does", "played"],
+    ["トムは昨晩、自転車を使いましたか?", "Did Tom use his bicycle last night?", "did", "Tom", "use", "his", "bicycle", "last", "night", "uses"],
+    ["あなたは先週、図書館に行きましたか?", "Did you visit the library last week?", "did", "you", "visit", "the", "library", "last", "week", "visited"],
+    ["彼はいつ、学校に行きましたか?", "When did he go to school?", "when", "did", "he", "go", "to", "school", "goes", "went"],
+    ["あなたはどのくらい、アメリカに滞在しましたか?", "How long did you stay in US?", "how", "long", "did", "you", "stay", "in", "US", "stayed"]
+
+]
