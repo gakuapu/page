23da631ee6c16eabdf40e62e9ba2247a178054ac
{gakuapu} <{dotddot.info@gmail.com}>
2024-11-30 19:33:21 +0900

    g6

---
+++ grammar/mondai_g1_6.js
@@ -0,0 +1,15 @@
+const $list = [
+
+    ["私は（今）手紙を書いています。", "I am writing a letter.", "I", "am", "writing", "a", "letter", "writeing", "is", "are"],
+    ["私の妹は（今）ケーキを作っています。", "My sister is making a cake.", "my", "sister", "is", "making", "a", "cake", "makes", "makeing"],
+    ["彼らは（今）泳いでいます。", "They are swimming.", "they", "are", "swimming", "doing", "swiming", "is", "swim", "am"],
+    ["トムは（今）あなたを待っていません。", "Tom is not waiting for you.", "Tom", "is", "not", "waiting", "for", "you", "are", "wait"],
+    ["私たちは（今）英語を勉強していません。", "We are not studying English.", "we", "are", "not", "studying", "English", "an", "studing", "study"],
+    ["私は（今）学校に向かっていません。", "I'm not going to school.", "I'm", "not", "going", "to", "school", "I", "doing", "go"],
+    ["彼は（今）トムと話していますか?", "Is he talking with Tom?", "is", "he", "talking", "with", "Tom", "are", "talk", "does"],
+    ["あなたは（今）写真を撮っていますか?", "Are you taking a picture?", "are", "you", "taking", "a", "picture", "is", "takeing", "take"],
+    ["あなたは（今）宿題をしていますか?", "Are you doing your homework?", "are", "you", "doing", "your", "homework", "do", "is", "am"],
+    ["あなたは（今）何を作っていますか?", "What are you making?", "what", "are", "you", "making", "make", "is", "makeing", "do"],
+    ["彼は（今）どこを走っていますか?", "Where is he running?", "where", "is", "he", "running", "doing", "runing", "does", "run"]
+
+]
