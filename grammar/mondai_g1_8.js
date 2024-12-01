d5f1fa312434c98d1b7a278b7c2ec2cc433d0816
{gakuapu} <{dotddot.info@gmail.com}>
2024-12-01 14:25:36 +0900

    g8

---
+++ grammar/mondai_g1_8.js
@@ -0,0 +1,15 @@
+const $list = [
+
+    ["私は先週の金曜日、京都にいました。", "I was in Kyoto last Friday.", "I", "was", "in", "Kyoto", "last", "Friday", "were", "am"],
+    ["彼女は昨晩、とても疲れていました。", "She was very tired last night.", "she", "was", "very", "tired", "last", "night", "were", "is"],
+    ["トムと私は幸せでした。", "Tom and I were happy.", "Tom", "and", "I", "were", "happy", "was", "happied", "are"],
+    ["彼は昨日、忙しくありませんでした。", "He was not busy yesterday.", "he", "was", "not", "busy", "yesterday", "were", "is", "busyed"],
+    ["私はサッカーチームに入っていませんでした。", "I wasn't on the soccer team.", "I", "wasn't", "on", "the", "soccer", "team", "weren't", "were"],
+    ["私の自転車は赤ではありませんでした。", "My bicycle was not red.", "my", "bicycle", "was", "not", "red", "were", "did", "weren't"],
+    ["あなたは昨日、家にいましたか?", "Were you at home yesterday?", "were", "you", "at", "home", "yesterday", "are", "was", "a"],
+    ["その授業はあなたには簡単でしたか?", "Was the class easy for you?", "was", "the", "class", "easy", "for", "you", "were", "is"],
+    ["昨晩は雨でしたか?", "Was it rainy last night?", "was", "it", "rainy", "last", "night", "were", "is", "rains"],
+    ["あなたはお昼にどこにいましたか?", "Where were you at noon?", "where", "were", "you", "at", "noon", "was", "are", "a"],
+    ["彼らの朝食は何でしたか?", "What was their breakfast?", "what", "was", "their", "breakfast", "were", "did", "is", "are"]
+
+]
