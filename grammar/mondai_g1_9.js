265f41a465037e4a4816cac2867316a78cd27f94
{gakuapu} <{dotddot.info@gmail.com}>
2024-12-05 23:44:40 +0900

    g9

---
+++ grammar/mondai_g1_9.js
@@ -0,0 +1,14 @@
+const $list = [
+
+    ["駅の近くにスーパーがあります。", "There is a supermarket near the station.", "there", "is", "a", "supermarket", "near", "the", "station", "are"],
+    ["リンゴがたくさんあります。", "There are a lot of apples.", "there", "are", "a", "lot", "of", "apples", "is", "an"],
+    ["私の家の近くに、図書館がありました。", "There was a library near my house.", "there", "was", "a", "library", "near", "my", "house", "is"],
+    ["私たちの学校にプールはありません。", "There isn't a pool in our school.", "there", "isn't", "a", "pool", "in", "our", "school", "not"],
+    ["空には、全く雲がありません。", "There aren't any clouds in the sky.", "there", "aren't", "any", "clouds", "in", "the", "sky", "isn't"],
+    ["通りには、あまり車がいませんでした。", "There weren't many cars on the street.", "there", "weren't", "many", "cars", "on", "the", "street", "wasn't"],
+    ["あなたの部屋にはテレビがありますか?", "Is there a TV in your room?", "is", "there", "a", "TV", "in", "your", "room", "are"],
+    ["この近くに公園はありますか?", "Is there a park near here?", "is", "there", "a", "park", "near", "here", "are", "this"],
+    ["お祭りには、たくさんの人がいましたか?", "Were there many people at the festival?", "were", "there", "many", "people", "at", "the", "festival", "are"],
+    ["あなたの学校にはいくつ、教室がありますか?", "How many classrooms are there in your school?", "how", "many", "classrooms", "are", "there", "in", "your", "school"]
+
+]
