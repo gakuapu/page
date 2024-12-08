73631aa031b6c4c4c56f6f9b6baf60349dcd8d20
{gakuapu} <{dotddot.info@gmail.com}>
2024-12-08 18:59:09 +0900

    g2_1

---
+++ grammar/mondai_g2_1.js
@@ -0,0 +1,14 @@
+const $list = [
+
+    ["私は次の夏、海に行くつもりです。", "I will go to the sea next summer.", "I", "will", "go", "to", "the", "sea", "next", "summer"],
+    ["彼女は7時に帰るつもりです。", "She will go home at seven.", "she", "will", "go", "home", "at", "seven", "goes", "is"],
+    ["彼は明日、釣りに行きます。", "He will go fishing tomorrow.", "he", "will", "go", "fishing", "tomorrow", "goes", "is", "be"],
+    ["私たちの先生は、今日は日本語を話さないつもりです。", "Our teacher won't speak Japanese today.", "our", "teacher", "won't", "speak", "Japanese", "today", "speaks", "is"],
+    ["彼女はピアノを弾かないでしょう。", "She will not play the piano.", "she", "will", "not", "play", "the", "piano", "does", "plays"],
+    ["私たちは今週末、サッカーの練習をしません。", "We will not practice soccer this weekend.", "we", "will", "not", "practice", "soccer", "this", "weekend", "are"],
+    ["あなたたちはオーストラリアに行くつもりですか?", "Will you visit Australia?", "will", "you", "visit", "Australia", "do", "are", "an", "to"],
+    ["来週、晴れますか?", "Will it sunny next week?", "will", "it", "sunny", "next", "week", "is", "a", "does"],
+    ["あなたは今晩、どこで勉強するつもりですか?", "Where will you study tonight?", "where", "will", "you", "study", "tonight", "are", "do", "studying"],
+    ["彼は次の日曜日、何をする予定ですか?", "What will he do next Sunday?", "what", "will", "he", "do", "next", "Sunday", "does", "is"]
+
+]
