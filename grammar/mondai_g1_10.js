0ac89324008d4feef0d5a520aad1377ef99986e3
{gakuapu} <{dotddot.info@gmail.com}>
2024-12-08 12:39:26 +0900

    g10

---
+++ grammar/mondai_g1_10.js
@@ -0,0 +1,14 @@
+const $list = [
+
+    ["私は昨夜7時にテレビを見ていました。", "I was watching TV at seven last night.", "I", "was", "watching", "TV", "at", "seven", "last", "night"],
+    ["あなたは私のとなりに座っていました。", "You were sitting next to me.", "you", "were", "sitting", "next", "to", "me", "I", "was"],
+    ["(「～の時」を後半にして)電話が鳴った時、私は料理をしていました。", "I was cooking when the phone rang.", "I", "was", "cooking", "when", "the", "phone", "rang", "cooked"],
+    ["その時、雨が降っていました。", "It was raining then.", "it", "was", "raining", "then", "is", "rained", "rain", "were"],
+    ["私は、その時、宿題をしていませんでした。", "I wasn't doing my homework at that time.", "I", "wasn't", "doing", "my", "homework", "at", "that", "time"],
+    ["その時、彼女は本を読んでいませんでした。", "She wasn't reading a book then.", "she", "wasn't", "reading", "a", "book", "then", "weren't", "didn't"],
+    ["彼はその時、寝ていましたか?", "Was he sleeping then?", "was", "he", "sleeping", "then", "were", "slept", "did", "is"],
+    ["(「～の時」を後半にして)あなたが彼らに電話した時、彼らは勉強していましたか?", "Were they studying when you called them?", "were", "they", "studying", "when", "you", "called", "them", "did"],
+    ["あなたはそこで何を料理していましたか?", "What were you cooking there?", "what", "were", "you", "cooking", "there", "did", "was", "cooked"],
+    ["彼女はその時、どこを走っていましたか?", "Where was she running then?", "where", "was", "she", "running", "then", "were", "are", "ran"]
+
+]
