afcb6163c4625d4f22e65c9fe3a683d68052d9f8
{gakuapu} <{dotddot.info@gmail.com}>
2024-12-21 17:27:13 +0900

    g2_2

---
+++ grammar/mondai_g2_2.js
@@ -0,0 +1,14 @@
+const $list = [
+
+    ["私は明日、学校に行く予定です。", "I am going to go to school tomorrow.", "I", "am", "going", "to", "go", "to", "school", "tomorrow"],
+    ["彼女は夕食を作る予定です。", "She is going to cook dinner.", "she", "is", "going", "to", "cook", "dinner", "goes", "cooks"],
+    ["彼は放課後、勉強をするつもりです。", "He is going to study after school.", "he", "is", "going", "to", "study", "after", "school", "studies"],
+    ["私たちはここで昼食を取る予定はありません。", "We're not going to have lunch here.", "we're", "not", "going", "to", "have", "lunch", "here", "we"],
+    ["明日は晴れなさそうです。", "It is not going to be sunny tomorrow.", "it", "is", "not", "going", "to", "be", "sunny", "tomorrow"],
+    ["彼はその本を読むつもりはありません。", "He isn't going to read the book.", "he", "isn't", "going", "to", "read", "the", "book", "doesn't"],
+    ["あなたは京都に滞在する予定ですか?", "Are you going to stay in Kyoto?", "are", "you", "going", "to", "stay", "in", "Kyoto", "do"],
+    ["彼女はスピーチをする予定ですか?", "Is she going to make a speech?", "is", "she", "going", "to", "make", "a", "speech", "makes"],
+    ["あなたは今晩、何をする予定ですか?", "What are you going to do tonight?", "what", "are", "you", "going", "to", "do", "tonight", "doing"],
+    ["彼は何を勉強する予定ですか?", "What is he going to study?", "what", "is", "he", "going", "to", "study", "studies", "be"]
+
+]
