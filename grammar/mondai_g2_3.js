4943f25fb5ec911f5dee147257302bb41f27137d
{gakuapu} <{dotddot.info@gmail.com}>
2024-12-22 16:39:02 +0900

    g2_3, b2_3a, 3b

---
+++ grammar/mondai_g2_3.js
@@ -0,0 +1,24 @@
+const $list = [
+
+    ["彼は昨日、疲れているように見えました。", "He looked tired yesterday.", "he", "looked", "tired", "yesterday", "look", "I", "was", "looks"],
+    ["そのテストは簡単そうには見えませんでした。", "The test didn't look easy.", "the", "test", "didn't", "look", "easy", "wasn't", "looked", "looking"],
+    ["彼女は来年、私たちに数学を教える予定です。", "She will teach us math next year.", "she", "will", "teach", "us", "math", "next", "year", "teaches"],
+    ["私の父は私に帽子を買ってくれました。", "My father bought me a hat.", "my", "father", "bought", "me", "a", "hat", "I", "him"],
+    ["彼は私に写真を見せました。", "He showed me a picture.", "he", "showed", "me", "a", "picture", "shows", "him", "show"],
+    ["私は彼女に自転車をあげました。", "I gave her a bicycle.", "I", "gave", "her", "a", "bicycle", "she", "give", "me"],
+    ["彼女はあなたにその話をしましたか?", "Did she tell you the story?", "did", "she", "tell", "you", "the", "story", "told", "does"],
+    ["あなたは私にアドバイスできますか?", "Can you give me advice?", "can", "you", "give", "me", "advice", "do", "I", "are"],
+    ["彼女は私たちに何を持って来るつもりですか?", "What will she bring us?", "what", "will", "she", "bring", "us", "brings", "do", "her"],
+    ["あなたはいつ、彼女に英語を教えましたか?", "When did you teach her English?", "when", "did", "you", "teach", "her", "English", "taught", "were"],
+    ["私たちのコーチはチームを強くしました。", "Our coach made the team strong.", "our", "coach", "made", "the", "team", "strong", "was", "make"],
+    ["彼の父親は彼の息子をTomと名付けました。", "His father named his son Tom.", "his", "father", "named", "his", "son", "Tom", "was", "name"],
+    ["その新しいルールは彼らを悲しませるでしょう。", "The new rule will make them sad.", "the", "new", "rule", "will", "make", "them", "sad", "is"],
+    ["人々は、この都市を江戸と呼んでいました。", "People called this city Edo.", "people", "called", "this", "city", "Edo", "did", "were", "call"],
+    ["その試合は私たちをワクワクさせませんでした。", "The game didn't make us excited.", "the", "game", "didn't", "make", "us", "excited", "wasn't", "made"],
+    ["太陽は私たちを暖かくしないでしょう。", "The sun won't make us warm.", "the", "sun", "won't", "make", "us", "warm", "makes", "is"],
+    ["彼はそのネコをMikeと呼びますか?", "Does he call the cat Mike?", "does", "he", "call", "the", "cat", "Mike", "is", "calls"],
+    ["その知らせは彼を怒らせましたか?", "Did the news make him angry?", "did", "the", "news", "make", "him", "angry", "was", "made"],
+    ["あなたたちはその料理を何と呼びますか?", "What do you call the dish?", "what", "do", "you", "call", "the", "dish", "are", "is"],
+    ["彼女はその鳥に何と名前を付けましたか?", "What did she name the bird?", "what", "did", "she", "name", "the", "bird", "named", "was"]
+
+]
