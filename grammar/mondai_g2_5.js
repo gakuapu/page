bdea76cab89d0daec636e230418b211fe436bb02
{gakuapu} <{dotddot.info@gmail.com}>
2024-12-29 11:26:39 +0900

    g2_5

---
+++ grammar/mondai_g2_5.js
@@ -0,0 +1,14 @@
+const $list = [
+
+    ["彼女は京都に行くと決めました。", "She decided to go to Kyoto.", "she", "decided", "to", "go", "to", "Kyoto", "went", "goes"],
+    ["彼はドアを開けようとしました。", "He tried to open the door.", "he", "tried", "to", "open", "the", "door", "opens", "opened"],
+    ["私は芸術家になりたいです。", "I want to be an artist.", "I", "want", "to", "be", "an", "artist", "am", "a"],
+    ["彼女は踊らないと決めました。", "She decided not to dance.", "she", "decided", "not", "to", "dance", "didn't", "decide", "dances"],
+    ["私は朝食を食べる時間がありませんでした。", "I didn't have time to eat breakfast.", "I", "didn't", "have", "time", "to", "eat", "breakfast", "not"],
+    ["彼は何か食べるものを持っています。", "He has something to eat.", "he", "has", "something", "to", "eat", "have", "eats", "a"],
+    ["彼は今日やる宿題がありません。", "He doesn't have his homework to do today.", "he", "doesn't", "have", "his", "homework", "to", "do", "today"],
+    ["私は泳ぐために早く起きました。", "I got up early to swim.", "I", "got", "up", "early", "to", "swim", "swam", "get"],
+    ["彼はその知らせを聞いて驚きました。", "He was surprised to hear the news.", "he", "was", "surprised", "to", "hear", "the", "news", "heard"],
+    ["あなたは勉強するために図書館に行きましたか?", "Did you go to the library to study?", "did", "you", "go", "to", "the", "library", "to", "study"]
+
+]
