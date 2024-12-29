7bb540eca04521b8edd76cab2ec9b9339682da92
{gakuapu} <{dotddot.info@gmail.com}>
2024-12-30 00:22:48 +0900

    b2_6

---
+++ grammar/mondai_g2_6.js
@@ -0,0 +1,14 @@
+const $list = [
+
+    ["(主語をitにして)速く走ることは、彼には簡単です。", "It is easy for him to run fast.", "it", "is", "easy", "for", "him", "to", "run", "fast"],
+    ["(主語をitにして)もっと練習することが彼には重要です。", "It is important for him to practice more.", "it", "is", "important", "for", "him", "to", "practice", "more"],
+    ["(主語をitにして)英語を話すことは私には難しかった。", "It was difficult for me to speak English.", "it", "was", "difficult", "for", "me", "to", "speak", "English"],
+    ["(主語をitにして)スキーをすることは、あなたには簡単だろう。", "It will be easy for you to ski.", "it", "will", "be", "easy", "for", "you", "to", "ski"],
+    ["(主語をitにして)ここで写真を撮っても構いません。", "It is possible to take pictures here.", "it", "is", "possible", "to", "take", "pictures", "here", "for"],
+    ["(主語をitにして)彼女にとって、泳ぐことは面白くない。", "It is not fun for her to swim.", "it", "is", "not", "fun", "for", "her", "to", "swim"],
+    ["(主語をitにして)京都を訪れることは、彼には不可能ではなかった。", "It wasn't impossible for him to visit Kyoto.", "it", "wasn't", "impossible", "for", "him", "to", "visit", "Kyoto"],
+    ["(主語をitにして)本を読むことは、あなたにとってワクワクすることですか?", "Is it exciting for you to read books?", "is", "it", "exciting", "for", "you", "to", "read", "books"],
+    ["(主語をitにして)踊ることは、彼女にとって楽しいことですか?", "Is it fun for her to dance?", "is", "it", "fun", "for", "her", "to", "dance", "she"],
+    ["(主語をitにして)テレビを見ることは、あなたに必要なことでしたか?", "Was it necessary for you to watch TV?", "was", "it", "necessary", "for", "you", "to", "watch", "TV"]
+
+]
