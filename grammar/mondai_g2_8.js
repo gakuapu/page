a8d278ca62053fd0cfe8e5200ad1432fadb0430a
{gakuapu} <{dotddot.info@gmail.com}>
2025-01-05 16:00:59 +0900

    b2_8, g2_8

---
+++ grammar/mondai_g2_8.js
@@ -0,0 +1,14 @@
+const $list = [
+
+    ["彼は一生懸命サッカーの練習をする必要がある。", "He has to practice soccer hard.", "he", "has", "to", "practice", "soccer", "hard", "have", "practices"],
+    ["私は部屋を掃除しなければならなかった。", "I had to clean my room.", "I", "had", "to", "clean", "my", "room", "have", "cleaned"],
+    ["彼は両親に電話しないといけないだろう。", "He will have to call his parents.", "he", "will", "have", "to", "call", "his", "parents", "has"],
+    ["彼らは今日、学校に行く必要はありません。", "They don't have to go to school today.", "they", "don't", "have", "to", "go", "to", "school", "today"],
+    ["私は手紙を書く必要はなかった。", "I didn't have to write a letter.", "I", "didn't", "have", "to", "write", "a", "letter", "had"],
+    ["彼女は明日、そこに行く必要はないだろう。", "She won't have to go there.", "she", "won't", "have", "to", "go", "there", "will", "doesn't"],
+    ["彼女はここで昼食を取らないといけませんか?", "Does she have to eat lunch here?", "does", "she", "have", "to", "eat", "lunch", "here", "has"],
+    ["彼は日本語を習う必要がありましたか?", "Did he have to learn Japanese?", "did", "he", "have", "to", "learn", "Japanese", "does", "had"],
+    ["彼女は明日、あなたを手伝わなければなりませんか?", "Will she have to help you tomorrow?", "will", "she", "have", "to", "help", "you", "tomorrow", "has"],
+    ["彼女は何を勉強しないといけませんか?", "What does she have to study?", "what", "does", "she", "have", "to", "study", "has", "studies"]
+
+]
