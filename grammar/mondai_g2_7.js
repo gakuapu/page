ae4452ea660b94eda7b6497cd4f51952d5b0107b
{gakuapu} <{dotddot.info@gmail.com}>
2025-01-05 13:42:58 +0900

    b2_7, g2_7

---
+++ grammar/mondai_g2_7.js
@@ -0,0 +1,14 @@
+const $list = [
+
+    ["私は家族のために朝食を作らないといけません。", "I must make breakfast for my family.", "I", "must", "make", "breakfast", "for", "my", "family", "a"],
+    ["彼はもっと英語を勉強しないといけない。", "He must study English more.", "he", "must", "study", "English", "more", "studies", "an", "is"],
+    ["その部屋では静かにしないといけません。", "You must be quiet in the room.", "you", "must", "be", "quiet", "in", "the", "room", "are"],
+    ["これは彼女のカバンに違いない。", "This must be her bag.", "this", "must", "be", "her", "bag", "is", "a", "the"],
+    ["子供はこのボタンに触ってはいけません。", "A child must not touch this button.", "a", "child", "must", "not", "touch", "this", "button", "touches"],
+    ["その川で泳いではいけません。", "We must not swim in the river.", "we", "must", "not", "swim", "in", "the", "river", "are"],
+    ["病院の中で走ってはいけません。", "You mustn't run in the hospital.", "you", "mustn't", "run", "in", "the", "hospital", "must", "do"],
+    ["彼女はすぐにここを出発しないといけませんか?", "Must she leave here soon?", "must", "she", "leave", "here", "soon", "do", "does", "leaves"],
+    ["彼はそれにもう一度挑戦しないといけませんか?", "Must he try it again?", "must", "he", "try", "it", "again", "does", "tries", "do"],
+    ["私はパーティーに何を持って行かないといけませんか?", "What must I bring to the party?", "what", "must", "I", "bring", "to", "the", "party", "do"]
+
+]
