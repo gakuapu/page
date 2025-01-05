cb6b1efc60f892d6426526e379af133e75488152
{gakuapu} <{dotddot.info@gmail.com}>
2025-01-05 19:43:12 +0900

    b2_9, g2_9

---
+++ grammar/mondai_g2_9.js
@@ -0,0 +1,14 @@
+const $list = [
+
+    ["私はその本を読み終えた。", "I finished reading the book.", "I", "finished", "reading", "the", "book", "to", "read", "finish"],
+    ["彼はピアノを弾くことが好きです。", "He likes playing the piano.", "he", "likes", "playing", "the", "piano", "play", "plays", "like"],
+    ["毎日英語を勉強することは重要です。", "Studying English every day is important.", "studying", "English", "every", "day", "is", "important", "study", "are"],
+    ["私は、そのゲームをすることはワクワクすると思います。", "I think playing the game is exciting.", "I", "think", "playing", "the", "game", "is", "exciting", "play"],
+    ["彼はテレビでスポーツを見ることは好きではありません。", "He doesn't like watching sports on TV.", "he", "doesn't", "like", "watching", "sports", "on", "TV", "watch"],
+    ["泳ぐことは私にとって楽しくありません。", "Swimming is not fun for me.", "swimming", "is", "not", "fun", "for", "me", "I", "am"],
+    ["彼女は音楽を聞くことは好きですか?", "Does she like listening to music?", "does", "she", "like", "listening", "to", "music", "is", "do"],
+    ["その川で泳ぐことは楽しいですか?", "Is swimming in the river fun?", "is", "swimming", "in", "the", "river", "fun", "swim", "do"],
+    ["あなたは昨晩、クッキーを作り終えましたか?", "Did you finish making cookies last night?", "did", "you", "finish", "making", "cookies", "last", "night", "make"],
+    ["あなたは何時に数学の勉強を始めますか?", "What time do you start studying math?", "what", "time", "do", "you", "start", "studying", "math", "study"]
+
+]
