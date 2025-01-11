8874e6c5660d10cee7bccc28a59d9d247ae478d5
{gakuapu} <{dotddot.info@gmail.com}>
2025-01-11 19:46:11 +0900

    b2_11, g2_11

---
+++ grammar/mondai_g2_11.js
@@ -0,0 +1,14 @@
+const $list = [
+
+    ["(主語から始めて)日本では、野球はバレーボールより人気があります。", "Baseball is more popular than volleyball in Japan.", "baseball", "is", "more", "popular", "than", "volleyball", "in", "Japan"],
+    ["彼は私より一生懸命ピアノを練習しました。", "He practiced the piano harder than I.", "he", "practiced", "the", "piano", "harder", "than", "I", "more"],
+    ["彼はあなたより速く泳ぐことができます。", "He can swim faster than you.", "he", "can", "swim", "faster", "than", "you", "more", "fast"],
+    ["私は弟より早く起きました。", "I got up earlier than my brother.", "I", "got", "up", "earlier", "than", "my", "brother", "more"],
+    ["あなたの車は私の車より新しいと思う。", "I think your car is newer than mine.", "I", "think", "your", "car", "is", "newer", "than", "mine"],
+    ["彼はこのテストのために、より一生懸命、英語を勉強しました。", "He studied English harder for this exam.", "he", "studied", "English", "harder", "for", "this", "exam", "than"],
+    ["あなたのチームは私のチームより強いですか?", "Is your team stronger than mine?", "is", "your", "team", "stronger", "than", "mine", "my", "more"],
+    ["彼女はネコより犬が好きですか?", "Does she like dogs more than cats?", "does", "she", "like", "dogs", "more", "than", "cats", "likes"],
+    ["今日は昨日より暖かくなりそうですか?", "Will it be warmer today than yesterday?", "will", "it", "be", "warmer", "today", "than", "yesterday", "warm"],
+    ["札幌とロンドンではどちらが寒いですか?", "Which is colder, Sapporo or London?", "which", "is", "colder", ",", "Sapporo", "or", "London", "and"]
+
+]
