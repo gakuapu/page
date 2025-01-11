a99a603051c11300784010d7b228a036821ea304
{gakuapu} <{dotddot.info@gmail.com}>
2025-01-11 15:52:08 +0900

    b2_10, g2_10

---
+++ grammar/mondai_g2_10.js
@@ -0,0 +1,14 @@
+const $list = [
+
+    ["彼女は私たちにケーキの作り方を見せてくれました。", "She showed us how to make a cake.", "she", "showed", "us", "how", "to", "make", "a", "cake"],
+    ["彼はいつ公園を走るか知っていました。", "He knew when to run in the park.", "he", "knew", "when", "to", "run", "in", "the", "park"],
+    ["先生は私たちに明日何を持って来るべきか言った。", "Our teacher told us what to bring tomorrow.", "our", "teacher", "told", "us", "what", "to", "bring", "tomorrow"],
+    ["彼女はどこで電車を降りればいいか知っています。", "She knows where to get off the train.", "she", "knows", "where", "to", "get", "off", "the", "train"],
+    ["彼は次に何を学ぶべきか分かりません。", "He doesn't know what to learn next.", "he", "doesn't", "know", "what", "to", "learn", "next", "learns"],
+    ["私は彼女に何と言うべきか分からなかった。", "I didn't know what to say to her.", "I", "didn't", "know", "what", "to", "say", "to", "her"],
+    ["彼はいつ、学校を出発するか分かっていますか?", "Does he know when to leave the school?", "does", "he", "know", "when", "to", "leave", "the", "school"],
+    ["どこを訪れればいいか彼に教えられますか?", "Can you tell him where to visit?", "can", "you", "tell", "him", "where", "to", "visit", "visits"],
+    ["あなたはどのバスに乗ればいいか分かりますか?", "Do you know which bus to take?", "do", "you", "know", "which", "bus", "to", "take", "a"],
+    ["彼女はこのゲームのやり方が分かりますか?", "Does she know how to play this game?", "does", "she", "know", "how", "to", "play", "this", "game"]
+
+]
