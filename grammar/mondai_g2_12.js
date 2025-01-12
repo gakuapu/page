73847ac0dc9bf91519de2450de6f56007c8631bc
{gakuapu} <{dotddot.info@gmail.com}>
2025-01-12 17:29:50 +0900

    b2_12, g2_12

---
+++ grammar/mondai_g2_12.js
@@ -0,0 +1,14 @@
+const $list = [
+
+    ["あの本は全ての中で最も面白い。", "That book is the most interesting of all.", "that", "book", "is", "the", "most", "interesting", "of", "all"],
+    ["彼は家族の中で一番背が高い。", "He is the tallest in his family.", "he", "is", "the", "tallest", "in", "his", "family", "most"],
+    ["私は野球が最もワクワクするスポーツだと思います。", "I think baseball is the most exciting sport.", "I", "think", "baseball", "is", "the", "most", "exciting", "sport"],
+    ["私の犬は一番早く走った。", "My dog ran the fastest.", "my", "dog", "ran", "the", "fastest", "most", "is", "fast"],
+    ["チーターは全ての動物の中で最も早く走ることができます。", "Cheetahs can run the fastest of all animals.", "cheetahs", "can", "run", "the", "fastest", "of", "all", "animals"],
+    ["彼は日本の中で京都が一番好きです。", "He likes Kyoto the best in Japan.", "he", "likes", "Kyoto", "the", "best", "in", "Japan", "is"],
+    ["彼女は日本で最も有名なアーティストです。", "She is the most famous artist in Japan.", "she", "is", "the", "most", "famous", "artist", "in", "Japan"],
+    ["(主語から始めて)数学は私にとって最も簡単な教科ではありません。", "Math isn't the easiest subject for me.", "math", "isn't", "the", "easiest", "subject", "for", "me", "subjects"],
+    ["トムはクラスで一番背が高いですか?", "Is Tom the tallest in his class?", "is", "Tom", "the", "tallest", "in", "his", "class", "most"],
+    ["彼はこのチームで一番うまい選手ですか?", "Is he the best player in this team?", "is", "he", "the", "best", "player", "in", "this", "team"]
+
+]
