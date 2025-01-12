957b2cf42ed7523dfe7b98f9b141e97d8ab3df57
{gakuapu} <{dotddot.info@gmail.com}>
2025-01-12 12:35:39 +0900

    bの例文

--- grammar/mondai_g2_8.js
+++ grammar/mondai_g2_8.js
@@ -5,7 +5,7 @@ const $list = [
     ["彼は両親に電話しないといけないだろう。", "He will have to call his parents.", "he", "will", "have", "to", "call", "his", "parents", "has"],
     ["彼らは今日、学校に行く必要はありません。", "They don't have to go to school today.", "they", "don't", "have", "to", "go", "to", "school", "today"],
     ["私は手紙を書く必要はなかった。", "I didn't have to write a letter.", "I", "didn't", "have", "to", "write", "a", "letter", "had"],
-    ["彼女は明日、そこに行く必要はないだろう。", "She won't have to go there.", "she", "won't", "have", "to", "go", "there", "will", "doesn't"],
+    ["彼女はそこに行く必要はないだろう。", "She won't have to go there.", "she", "won't", "have", "to", "go", "there", "will", "doesn't"],
     ["彼女はここで昼食を取らないといけませんか?", "Does she have to eat lunch here?", "does", "she", "have", "to", "eat", "lunch", "here", "has"],
     ["彼は日本語を習う必要がありましたか?", "Did he have to learn Japanese?", "did", "he", "have", "to", "learn", "Japanese", "does", "had"],
     ["彼女は明日、あなたを手伝わなければなりませんか?", "Will she have to help you tomorrow?", "will", "she", "have", "to", "help", "you", "tomorrow", "has"],
