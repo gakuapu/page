4172f755b84694c4380f97ebf2f3d0fc4892cf47
{gakuapu} <{dotddot.info@gmail.com}>
2025-01-13 22:25:08 +0900

    b_14, g_14

---
+++ grammar/mondai_g2_14.js
@@ -0,0 +1,14 @@
+const $list = [
+
+    ["この昼食は彼女の友人によって作られました。", "This lunch was cooked by her friend.", "this", "lunch", "was", "cooked", "by", "her", "friend", "cook"],
+    ["この写真は京都で撮られました。", "This picture was taken in Kyoto.", "this", "picture", "was", "taken", "in", "Kyoto", "by", "took"],
+    ["その病院は昨年建てられました。", "The hospital was built last year.", "the", "hospital", "was", "built", "last", "year", "by", "builded"],
+    ["この映画は世界で観られるだろう。", "This movie will be watched in the world.", "this", "movie", "will", "be", "watched", "in", "the", "world"],
+    ["彼女はパーティに招待されなかった。", "She wasn't invited to the party.", "she", "wasn't", "invited", "to", "the", "party", "invite", "by"],
+    ["この車は日本製ではありません。", "This car isn't made in Japan.", "this", "car", "isn't", "made", "in", "Japan", "be", "doesn't"],
+    ["昨日のお祭りは学生たちによって開催されましたか?", "Was yesterday's festival held by students?", "was", "yesterday's", "festival", "held", "by", "students", "did", "be"],
+    ["新しい博物館がここに建てられる予定ですか?", "Will a new museum be built here?", "will", "a", "new", "museum", "be", "built", "here", "build"],
+    ["その図書館は毎日、いつ閉められますか?", "When is the library closed every day?", "when", "is", "the", "library", "closed", "every", "day", "does"],
+    ["カナダではいくつの言語が話されていますか?", "How many languages are spoken in Canada?", "how", "many", "languages", "are", "spoken", "in", "Canada", "does"]
+
+]
