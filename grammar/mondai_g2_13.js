d761b2427b9e959b9ffd3fff07422e903c1fd588
{gakuapu} <{dotddot.info@gmail.com}>
2025-01-13 15:31:31 +0900

    b2_13, g2_13

---
+++ grammar/mondai_g2_13.js
@@ -0,0 +1,14 @@
+const $list = [
+
+    ["この車はあの車と同じくらい速い。", "This car is as fast as that one.", "this", "car", "is", "as", "fast", "as", "that", "one"],
+    ["この犬はあのネコと同じくらいかわいい。", "This dog is as cute as that cat.", "this", "dog", "is", "as", "cute", "as", "that", "cat"],
+    ["彼はあなたと同じくらい上手にピアノを弾きます。", "He plays the piano as well as you.", "he", "plays", "the", "piano", "as", "well", "as", "you"],
+    ["あなたはできるだけ早く家に帰らないといけません。", "You must go home as soon as possible.", "you", "must", "go", "home", "as", "soon", "as", "possible"],
+    ["私はできるだけ英語を話しました。", "I spoke English as much as I could.", "I", "spoke", "English", "as", "much", "as", "I", "could"],
+    ["私のケーキはあなたのほどおいしくない。", "My cake isn't as delicious as yours.", "my", "cake", "isn't", "as", "delicious", "as", "yours", "you"],
+    ["私はあなたほど一生懸命、英語を勉強しませんでした。", "I didn't study English as hard as you.", "I", "didn't", "study", "English", "as", "hard", "as", "you"],
+    ["彼女はあなたのお兄さんと同じくらいの年齢ですか?", "Is she as old as your brother?", "is", "she", "as", "old", "as", "your", "brother", "older"],
+    ["(同じ意味の文章に)I can't dance as well as him.", "He can dance better than I.", "he", "can", "dance", "better", "than", "I", "well", "him"],
+    ["(同じ意味の文章に)English is not as difficult as math for me.", "Math is more difficult than English for me.", "math", "is", "more", "difficult", "than", "English", "for", "me"]
+
+]
