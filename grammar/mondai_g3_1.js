9a8aac4755070c7154a37ef65525b296e8c6101f
{gakuapu} <{dotddot.info@gmail.com}>
2025-01-26 22:03:21 +0900

    b3_1, g3_1

---
+++ grammar/mondai_g3_1.js
@@ -0,0 +1,14 @@
+const $list = [
+
+    ["私は英語で手紙を書いたことがあります。", "I have written a letter in English.", "I", "have", "written", "a", "letter", "in", "English", "wrote"],
+    ["私は以前、その映画を見たことがあります。", "I have watched that movie before.", "I", "have", "watched", "that", "movie", "before", "watch", "had"],
+    ["彼女はスペインに行ったことがあります。", "She has been to Spain.", "she", "has", "been", "to", "Spain", "have", "was", "gone"],
+    ["私はその有名な本を一度も読んだことがありません。", "I have never read the famous book.", "I", "have", "never", "read", "the", "famous", "book", "didn't"],
+    ["(neverを使って)彼は一度も寿司を食べたことがありません。", "He has never eaten Sushi.", "he", "has", "never", "eaten", "Sushi", "not", "eat", "eats"],
+    ["(everを使って)彼女は今までにスキーをしたことがありますか?", "Has she ever tried skiing?", "has", "she", "ever", "tried", "skiing", "does", "have", "tries"],
+    ["(everを使って)あなたは今までにギターを弾いたことがありますか?", "Have you ever played the guitar?", "have", "you", "ever", "played", "the", "guitar", "did", "had"],
+    ["あなたは何回、そのゲームをしたことがありますか?", "How many times have you played the game?", "how", "many", "times", "have", "you", "played", "the", "game"],
+    ["(everを使って)これまでに何語を勉強したことがありますか?", "What languages have you ever studied?", "what", "languages", "have", "you", "ever", "studied", "did", "had"],
+    ["私は先月、ブラジルに行きました。", "I went to Brazil last month.", "I", "went", "to", "Brazil", "last", "month", "have", "been"]
+
+]
