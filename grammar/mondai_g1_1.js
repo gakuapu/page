d9ea36e8fa83d218eb5e1afb9b9a0769f3a799f6
{gakuapu} <{dotddot.info@gmail.com}>
2026-02-14 12:05:07 +0900

    g1_1

--- grammar/mondai_g1_1.js
+++ grammar/mondai_g1_1.js
@@ -1,4 +1,4 @@
-const $list = [
+const $practiceSentence = [
 
     ["あなたは私の友達です。", "You are my friend.", "you", "are", "my", "friend", "am", "is", "I", "a"],
     ["私の友人は教室にいます。", "My friend is in the classroom.", "my", "friend", "is", "in", "the", "classroom", "am", "are"],
@@ -11,4 +11,19 @@ const $list = [
     ["あなたの学校はとても大きいですか?", "Is your school very big?", "is", "your", "school", "very", "big", "am", "are", "a"],
     ["あなたたちは今、とても忙しいですか?", "Are you very busy now?", "are", "you", "very", "busy", "now", "am", "is", "your"]
     
+]
+
+const $basicSentence = [
+
+    ["あなたは私の友達です。", "You are my friend."],
+    ["私の友人は教室にいます。", "My friend is in the classroom."],
+    ["あれは鳥です。", "That is a bird."],
+    ["トムと私はオーストラリア出身です。", "Tom and I are from Australia."],
+    ["私は彼の弟ではありません。", "I am not his brother."],
+    ["私たちは学生ではありません。", "We are not students."],
+    ["これは私の教科書ではありません。", "This is not my textbook."],
+    ["彼女はあなたのお姉さんですか?", "Is she your sister?"],
+    ["あなたの学校はとても大きいですか?", "Is your school very big?"],
+    ["あなたたちは今、とても忙しいですか?", "Are you very busy now?"]
+
 ]
