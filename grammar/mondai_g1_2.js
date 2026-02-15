7a0b2446cdfae514932d800a70d5de0bced56b2a
{gakuapu} <{dotddot.info@gmail.com}>
2026-02-15 11:44:10 +0900

    g1_2

--- grammar/mondai_g1_2.js
+++ grammar/mondai_g1_2.js
@@ -1,4 +1,4 @@
-const $list = [
+const $practiceSentence = [
 
     ["私は電車で学校に行きます。", "I go to school by train.", "I", "go", "to", "school", "by", "train", "am", "I'm"],
     ["彼らは毎日英語を勉強します。", "They study English every day.", "they", "study", "English", "every", "day", "am", "are", "is"],
@@ -10,5 +10,20 @@ const $list = [
     ["あなたは放課後、本を読みますか?", "Do you read a book after school?", "do", "you", "read", "a", "book", "after", "school", "are"],
     ["あなたは野球が好きですか?", "Do you like baseball?", "do", "you", "like", "baseball", "are", "is", "am", "a"],
     ["彼らは東京に住んでいますか?", "Do they live in Tokyo?", "do", "they", "live", "in", "Tokyo", "are", "is", "am"]
-    
+
+]
+
+const $basicSentence = [
+
+    ["私は電車で学校に行きます。", "I go to school by train."],
+    ["彼らは毎日英語を勉強します。", "They study English every day."],
+    ["私たちは水曜日に英語を勉強します。", "We study English on Wednesdays."],
+    ["私は日曜日は勉強しません。", "I don't study on Sundays."],
+    ["彼らは日本語を話しません。", "They don't speak Japanese."],
+    ["トムと私は東京に住んでいません。", "Tom and I don't live in Tokyo."],
+    ["あなたは走るのが好きですか?", "Do you like running?"],
+    ["あなたは放課後、本を読みますか?", "Do you read a book after school?"],
+    ["あなたは野球が好きですか?", "Do you like baseball?"],
+    ["彼らは東京に住んでいますか?", "Do they live in Tokyo?"]
+
 ]
